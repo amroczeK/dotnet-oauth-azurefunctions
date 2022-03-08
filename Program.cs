5fbce03c1f8dafe89fdaae5c88b6095366ffd3b9
d883695 <adrian.mroczek@team.telstra.com>
2022-03-08 15:01:22 +0800

    dependency injection for IOptions

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Azure.Functions.Worker.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Solution.RuralWater.AZF.Options;
 
 namespace Solution.RuralWater.AZF
 {
@@ -11,7 +13,18 @@ namespace Solution.RuralWater.AZF
         {
             var host = new HostBuilder()
                 .ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices(services => {
+                    services.AddOptions<Secrets>().Configure<IConfiguration>((settings, configuration) =>
+                    {
+                        configuration.GetSection(nameof(Secrets)).Bind(settings);
+                    });
+                    services.AddOptions<Config>().Configure<IConfiguration>((settings, configuration) =>
+                    {
+                        configuration.GetSection(nameof(Config)).Bind(settings);
+                    });
+                })
                 .Build();
+                
 
             host.Run();
         }
