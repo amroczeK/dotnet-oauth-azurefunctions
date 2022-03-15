e247beb811ac1b763176b9e6cf0ad122aabf12a7
Adrian Mroczek <adrian.mroczek@team.telstra.com>
2022-03-15 13:48:33 +0800

    re-arrange requests

--- Functions/CellularDeviceHistory.cs
+++ Functions/CellularDeviceHistory.cs
@@ -28,6 +28,64 @@ namespace Solution.RuralWater.AZF.Functions
             _queryService = queryService;
         }
 
+        [Function("GetCellularDeviceHistoryRdmw")]
+        public async Task<HttpResponseData> GetCellularDeviceHistoryRdmw(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "data/cellular-device-history/rdmw")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("Rdmw");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            // Validate Authorization header and ApiKey
+            AuthorizationHelper authorizationHelper = new AuthorizationHelper(logger, _secrets);
+            var validate = authorizationHelper.ValidateApiKey(req.Headers);
+
+            if (!validate.Valid)
+            {
+                response.StatusCode = HttpStatusCode.BadRequest;
+                await response.WriteStringAsync(validate.Message);
+                return response;
+            }
+
+            // Parse query parameters
+            var queryDictionary = QueryHelpers.ParseQuery(req.Url.Query);
+
+            // Validate required query parameters
+            //var queryParams = new QueryParams();
+            response = await QueryParams.ValidateQueryParams(response, queryDictionary);
+            if (response.StatusCode == HttpStatusCode.BadRequest) return response;
+
+            // Required: Convert parameters to dynamic object because GraphQLRequest Variables expects Anonymous Type...
+            dynamic dynamicQueryParams = QueryParams.DictionaryToDynamic(queryDictionary);
+
+            var authenticationHelper = new AuthenticationHelper(logger, _authOptions, _secrets);
+            var result = await authenticationHelper.GetAccessToken();
+
+            try
+            {
+                logger.LogInformation("Querying {GraphQlUrl}", _authOptions.GraphQlUrl);
+
+                GraphQLHttpClient client = _queryService.CreateClient(result.AccessToken);
+
+                const string xdsName = Constants.CellularDeviceHistoryXdsName;
+                const string xdsViewName = "rdmw";
+                const string version = "v1";
+                GraphQLRequest request = _queryService.CreateRequest(xdsName, xdsViewName, version, dynamicQueryParams);
+
+                var data = await client.SendQueryAsync<CDHGraphQlResponse>(request);
+
+                await response.WriteAsJsonAsync(data.Data.cellularDeviceHistoryResponse);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Error occured querying GraphQL: {error}", ex.Message);
+                response.StatusCode = HttpStatusCode.InternalServerError;
+                return response;
+            }
+        }
+
         [Function("GetDevices")]
         public async Task<HttpResponseData> GetDevices(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "devices")] HttpRequestData req,
@@ -90,64 +148,5 @@ namespace Solution.RuralWater.AZF.Functions
                 return response;
             }
         }
-
-        [Function("GetCellularDeviceHistoryRdmw")]
-        public async Task<HttpResponseData> GetCellularDeviceHistoryRdmw(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "data/cellular-device-history/rdmw")] HttpRequestData req,
-            FunctionContext executionContext)
-        {
-            var logger = executionContext.GetLogger("Rdmw");
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-
-            // Validate Authorization header and ApiKey
-            AuthorizationHelper authorizationHelper = new AuthorizationHelper(logger, _secrets);
-            var validate = authorizationHelper.ValidateApiKey(req.Headers);
-
-            if (!validate.Valid)
-            {
-                response.StatusCode = HttpStatusCode.BadRequest;
-                await response.WriteStringAsync(validate.Message);
-                return response;
-            }
-
-            // Parse query parameters
-            var queryDictionary = QueryHelpers.ParseQuery(req.Url.Query);
-
-            // Validate required query parameters
-            //var queryParams = new QueryParams();
-            response = await QueryParams.ValidateQueryParams(response, queryDictionary);
-            if (response.StatusCode == HttpStatusCode.BadRequest) return response;
-
-            // Required: Convert parameters to dynamic object because GraphQLRequest Variables expects Anonymous Type...
-            dynamic dynamicQueryParams = QueryParams.DictionaryToDynamic(queryDictionary);
-
-            var authenticationHelper = new AuthenticationHelper(logger, _authOptions, _secrets);
-            var result = await authenticationHelper.GetAccessToken();
-
-            try
-            {
-                logger.LogInformation("Querying {GraphQlUrl}", _authOptions.GraphQlUrl);
-
-                GraphQLHttpClient client = _queryService.CreateClient(result.AccessToken);
-
-                const string xdsName = Constants.CellularDeviceHistoryXdsName;
-                const string xdsViewName = "rdmw";
-                const string version = "v1";
-                GraphQLRequest request = _queryService.CreateRequest(xdsName, xdsViewName, version, dynamicQueryParams);
-
-                var data = await client.SendQueryAsync<CDHGraphQlResponse>(request);
-
-                await response.WriteAsJsonAsync(data.Data.cellularDeviceHistoryResponse);
-                return response;
-            }
-            catch (Exception ex)
-            {
-                logger.LogError("Error occured querying GraphQL: {error}", ex.Message);
-                response.StatusCode = HttpStatusCode.InternalServerError;
-                return response;
-            }
-        }
-
     }
 }
--- Functions/Flow.cs
+++ Functions/Flow.cs
@@ -29,69 +29,6 @@ namespace Solution.RuralWater.AZF.Functions
             _queryService = queryService;
         }
 
-
-        [Function("GetMeasurements")]
-        public async Task<HttpResponseData> GetMeasurements(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "measurements")] HttpRequestData req,
-            FunctionContext executionContext)
-        {
-            var logger = executionContext.GetLogger("Rdmw");
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-
-            // Parse query parameters
-            var queryDictionary = QueryHelpers.ParseQuery(req.Url.Query);
-
-            var reqParams = QueryParams.ConvertDictionaryTo<MeasurementsReqParams>(queryDictionary);
-
-            object flowParams = new
-            {
-                accountId = _authOptions.AccountId,
-                //SiteId = reqParams.site_id,
-                //DeviceId = reqParams.device_id,
-                tz = "UTC"
-            };
-
-            Dictionary<string, object> test = QueryParams.ConvertObjectToDictionary(flowParams);
-
-            // Required: Convert parameters to dynamic object because GraphQLRequest Variables expects Anonymous Type...
-            dynamic dynamicQueryParams = QueryParams.DictionaryToDynamic(test);
-
-            // Get Bearer token using Password Credentials flow to be able to query GraphQL layer
-            var authenticationHelper = new AuthenticationHelper(logger, _authOptions, _secrets);
-            var result = await authenticationHelper.GetAccessToken();
-
-            if (result.AccessToken == null)
-            {
-                response.StatusCode = HttpStatusCode.BadRequest;
-                return response;
-            }
-
-            try
-            {
-                logger.LogInformation("Querying {GraphQlUrl}", _authOptions.GraphQlUrl);
-
-                GraphQLHttpClient client = _queryService.CreateClient(result.AccessToken);
-
-                const string xdsName = Constants.FlowXdsName;
-                const string xdsViewName = "rdmw";
-                const string version = "v1";
-                GraphQLRequest request = _queryService.CreateRequest(xdsName, xdsViewName, version, flowParams);
-
-                var data = await client.SendQueryAsync<FlowGraphQlResponse>(request);
-
-                await response.WriteAsJsonAsync(data.Data.flowResponse);
-                return response;
-            }
-            catch (Exception ex)
-            {
-                logger.LogError("Error occured querying GraphQL: {error}", ex.Message);
-                response.StatusCode = HttpStatusCode.InternalServerError;
-                return response;
-            }
-
-        }
-
         [Function("GetFlowRdmw")]
         public async Task<HttpResponseData> GetFlowRdmw(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "data/flow/rdmw")] HttpRequestData req,
@@ -142,6 +79,68 @@ namespace Solution.RuralWater.AZF.Functions
                 const string xdsViewName = "rdmw";
                 const string version = "v1";
                 GraphQLRequest request = _queryService.CreateRequest(xdsName, xdsViewName, version, dynamicQueryParams);
+
+                var data = await client.SendQueryAsync<FlowGraphQlResponse>(request);
+
+                await response.WriteAsJsonAsync(data.Data.flowResponse);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Error occured querying GraphQL: {error}", ex.Message);
+                response.StatusCode = HttpStatusCode.InternalServerError;
+                return response;
+            }
+
+        }
+
+        [Function("GetMeasurements")]
+        public async Task<HttpResponseData> GetMeasurements(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "measurements")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("Rdmw");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            // Parse query parameters
+            var queryDictionary = QueryHelpers.ParseQuery(req.Url.Query);
+
+            var reqParams = QueryParams.ConvertDictionaryTo<MeasurementsReqParams>(queryDictionary);
+
+            object flowParams = new
+            {
+                accountId = _authOptions.AccountId,
+                //SiteId = reqParams.site_id,
+                //DeviceId = reqParams.device_id,
+                tz = "UTC"
+            };
+
+            Dictionary<string, object> test = QueryParams.ConvertObjectToDictionary(flowParams);
+
+            // Required: Convert parameters to dynamic object because GraphQLRequest Variables expects Anonymous Type...
+            dynamic dynamicQueryParams = QueryParams.DictionaryToDynamic(test);
+
+            // Get Bearer token using Password Credentials flow to be able to query GraphQL layer
+            var authenticationHelper = new AuthenticationHelper(logger, _authOptions, _secrets);
+            var result = await authenticationHelper.GetAccessToken();
+
+            if (result.AccessToken == null)
+            {
+                response.StatusCode = HttpStatusCode.BadRequest;
+                return response;
+            }
+
+            try
+            {
+                logger.LogInformation("Querying {GraphQlUrl}", _authOptions.GraphQlUrl);
+
+                GraphQLHttpClient client = _queryService.CreateClient(result.AccessToken);
+
+                const string xdsName = Constants.FlowXdsName;
+                const string xdsViewName = "rdmw";
+                const string version = "v1";
+                GraphQLRequest request = _queryService.CreateRequest(xdsName, xdsViewName, version, flowParams);
 
                 var data = await client.SendQueryAsync<FlowGraphQlResponse>(request);
 
